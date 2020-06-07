877ab0c349863053f52cfa317820e342c08c48d0
jacKlinc <jackharding321@gmail.com>
2020-06-07 16:13:35 +0100

    Weighting system added
    From the downloaded data:
    - Listen times were grouped by artist and this
    helped in measuring artist relevance.
    - Number of listens were grouped by artist
    - Listen recency has begun but there are issues with subtracting the
    most recent data from each entry

--- secrets.py
+++
@@ -1,3 +0,0 @@
-"""Stored Spotify tokens"""
-spotify_token = "BQB3us8PHMiEExH2dtrnbyuayoqW4_K7mHE_AoP8Gc68ecuUdiw8npjOZmNPH4CAsmX2Tdb4XCKmrnc8fyrPHXMMob4A"
-spotify_user_id = "a2la4jzttvnov9au11hwf56z8"
