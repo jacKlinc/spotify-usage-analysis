bebbdf06a0e13e0770be800ee27945f8641b8c54
jacKlinc <jackharding321@gmail.com>
2020-07-07 18:33:51 +0100

    Added Genres Plot
    - Genre plotted with percent grading
    - 10 genres used with basic weighting
    - Refactored Nation, Age and Genres
    
    Added all to Streamlit

--- src/src_streamlit-app.py
+++ src/src_streamlit-app.py
@@ -35,6 +35,7 @@ def scrape_race(artist_name):
     else:
         return 'Unknown'
 
+## Gather and store functions
 @st.cache
 def make_df(response):
     """Pass results from Spotfy API call and return cleaned DataFrame"""
@@ -68,11 +69,80 @@ def spotify_connect(user_scope, redirect_uri, artist_limit, time_range):
             results = spotify_client.current_user_top_artists(time_range=r, limit=artist_limit)
         return results 
 
+## Grouping plot functions
+def get_country_groups(top50_df):
+    """Pass Top 50 artists and returns country group of those artists"""
+    # Remove 'n/a'
+    remove_nas = top50_df.loc[top50_df.country != 'n/a']
+    # Group artists into countries and sort
+    return remove_nas.groupby('country').artist.count().sort_values(ascending=False)
+
+def get_age_groups(top50_df):
+    """Pass Top 50 artists and returns age group of those artists"""
+    # Remove 'n/a'
+    cleaned_df = top50_df.loc[top50_df.age != 'n/a']
+
+    # Define age groups
+    bins= [0,15,24,35,50,110]
+    labels = ['0-15', '16-24', '25-35', '35-50', '50+']
+    return pd.cut(cleaned_df['age'], bins=bins, labels=labels, right=False)
+
+def grade_genre(genre, total):
+    """Pass genre and total of genres, returns percent graded genre"""
+    return (genre/total) * 100
+
+def get_genres(df):
+    """Pass artists df and returns graded genre dict"""
+    # Init genre values
+    rock=rap=pop=house=funk=folk=RandB=jazz=classical=electronic=0
+    # Loop over each artist
+    for genres in df.genres:
+        # Loop through artist's list of genres
+        for genre in genres:
+            if 'rap' in genre or 'hip hop' in genre:
+                rap+=1
+            elif 'pop' in genre:
+                pop+=1
+            elif 'rock' in genre:
+                rock+=1
+            elif 'funk' in genre:
+                funk+=1
+            elif 'house' in genre or 'techno' in genre:
+                house+=1
+            elif 'folk' in genre or 'country' in genre:
+                folk+=1
+            elif 'r&b' in genre or 'rhythm' in genre:
+                RandB+=1
+            elif 'jazz' in genre:
+                jazz+=1
+            elif 'classical' in genre:
+                classical+=1
+            elif 'electronic' in genre:
+                electronic+=1 
+    
+    # Find sum of all genres
+    total = rock+rap+pop+house+funk+folk+RandB+jazz+classical+electronic
+    
+    # Pass genre and total to percent grade
+    return {
+        'rap': grade_genre(rap, total),
+        'pop': grade_genre(pop, total),
+        'rock': grade_genre(rock, total),
+        'funk': grade_genre(funk, total),
+        'techno/house': grade_genre(house, total),
+        'country/folk':grade_genre(folk, total),
+        'r&b': grade_genre(RandB, total),
+        'jazz': grade_genre(jazz, total),
+        'classical': grade_genre(classical, total),
+        'electronic': grade_genre(electronic, total)
+    }
+
+
 scope = "user-top-read"
 redirect_uri = "http://localhost:8080"
 
 # Get top 10 artists for short, medium and long term
-results = spotify_connect(scope, redirect_uri, 10, ['short_term', 'medium_term', 'long_term'])
+results = spotify_connect(scope, redirect_uri, 50, ['short_term', 'medium_term', 'long_term'])
 
 # Make DataFrame    
 artists = make_df(results)
@@ -82,14 +152,9 @@ my_top50_artist_country = pd.read_json('../data/mytop50_artists.json')
 top500_artist_country = pd.read_json('../data/top500_spotify_artists.json')
 
 
-### Title 
+# Title 
 st.title("[names]'s Spotify Listening")
-
 'This is a peek at your listening trends'
-
-# Show head
-# st.write('## Spotify Artists DataFrame')
-# st.write(artists.head(10))
 
 # Plot Favourite Artists
 st.write('# Your Top 5')
@@ -101,15 +166,8 @@ st.pyplot()
 # Top artist
 artists.head(1).name.iloc[0]+' has the lead'
 
-# races = []
-# for artist in artists.name:
-#     races.append(scrape_race(artist))
-
-# st.write(races)
-
 # Plot nationalities
 st.write("# Where They're from")
-
 
 # Group artists into countries and sort
 countries_group = my_top50_artist_country.groupby('country').artist.count().sort_values(ascending=False)
@@ -124,24 +182,22 @@ countries_group.index[0], ' has the lead'
 st.write("# Age?")
 'Here are your favourite age groups'
 
-# Remove 'n/a'
-cleaned_df = my_top50_artist_country.loc[my_top50_artist_country.age != 'n/a']
-
-# Define age groups
-bins= [0,15,24,35,50,110]
-labels = ['0-15', '16-24', '25-35', '36-50', '50+']
-cleaned_df['AgeGroup'] = pd.cut(cleaned_df['age'], bins=bins, labels=labels, right=False)
-
-cleaned_df = cleaned_df.sort_values(by='age')
+age_groups = get_age_groups(my_top50_artist_country)
 
 fig, ax = plt.subplots(figsize=(12,5))
-ax.bar(cleaned_df['AgeGroup'], cleaned_df.index)
+ax.bar(age_groups, age_groups.index)
 st.pyplot()
 
-cleaned_df['AgeGroup'].max(), ' is in the lead'
+age_groups.max(), ' is in the lead'
 
 
 ### Plot genres
 st.write("# Genres")
 
-'You like', cleaned_df['AgeGroup'].max(), ' from',  countries_group.index[0],' in [genre]'
+genres = get_genres(artists)
+
+fig, ax = plt.subplots(figsize=(14,4))
+ax.bar(genres.keys(), genres.values())
+st.pyplot()
+
+'You like', age_groups.max(), ' from', countries_group.index[0],' in the ', max(genres, key=genres.get), 'genre'
